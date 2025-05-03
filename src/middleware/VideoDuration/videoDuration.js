16d73a41875af403c07ee7e77046be9dea19c1ab
Akash Shrestha <akashshrestha0399@gmail.com>
2025-05-03 13:15:54 +0530

    fix: update ffprobe installer and dependencies, adjust video duration middleware
    - Updated @ffprobe-installer/ffprobe from version 2.1.2 to 1.4.1 in package.json and package-lock.json.
    - Adjusted video duration middleware to use the new ffmpeg and ffprobe installers.
    - Updated various dependencies to their latest versions for improved stability and security.
    - Added new logging for recent API errors in error.log.

--- src/middleware/VideoDuration/videoDuration.js
+++ src/middleware/VideoDuration/videoDuration.js
@@ -32,10 +32,11 @@
 // module.exports = { getVideoDuration };
 
 const ffmpeg = require("fluent-ffmpeg");
-const ffprobe = require("ffprobe-static");
-const path = require("path");
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+const ffprobeInstaller = require("@ffprobe-installer/ffprobe");
 
-ffmpeg.setFfprobePath(ffprobe.path);
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+ffmpeg.setFfprobePath(ffprobeInstaller.path);
 
 const getVideoDuration = (filePath) => {
   return new Promise((resolve, reject) => {
@@ -48,3 +49,4 @@ const getVideoDuration = (filePath) => {
 };
 
 module.exports = { getVideoDuration };
+
